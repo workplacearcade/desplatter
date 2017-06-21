fcbebe7264c013660877b43ecc5ca83e0cc81a64
James McLaren <jamesmclaren555@gmail.com>
2017-06-22 08:13:23 +1000

    Tidied up desplat method

--- lib/arcade/desplatter.rb
+++ lib/arcade/desplatter.rb
@@ -3,15 +3,13 @@ require 'binding_of_caller'
 module Arcade
   module Desplatter
     def desplat(*args)
-      initialize_binding = binding.of_caller(1)
+      calling_binding = binding.of_caller(1)
 
-      method_name = caller_locations(1,1)[0].label.to_sym
+      calling_method = caller_locations(1,1)[0].label.to_sym
 
-      method(method_name).parameters.map do |_, param|
-        symbol = param.to_sym
-        param_value = initialize_binding.local_variable_get(symbol)
-        singleton_class.class_eval { attr_accessor symbol }
-        instance_variable_set("@#{param}", param_value)
+      method(calling_method).parameters.map do |_, param|
+        singleton_class.class_eval { attr_accessor param }
+        instance_variable_set("@#{param}", calling_binding.local_variable_get(param))
       end
     end
   end
