d5d8e0f2e6282f018b109665bdcfcf71b0da334e
James McLaren <jamesmclaren555@gmail.com>
2017-06-22 11:24:21 +1000

    Tidied up a bit based on Erin's feedback

--- lib/arcade/desplatter.rb
+++ lib/arcade/desplatter.rb
@@ -2,10 +2,10 @@ require 'binding_of_caller'
 
 module Arcade
   module Desplatter
-    def desplat(*args)
+    def desplat
       calling_binding = binding.of_caller(1)
 
-      calling_method = caller_locations(1,1)[0].label.to_sym
+      calling_method = caller_locations.first.label.to_sym
 
       method(calling_method).parameters.map do |_, param|
         singleton_class.class_eval { attr_accessor param }
