a2a9511747734a4920dd2f7e5c5f61b852638709
James McLaren <jamesmclaren555@gmail.com>
2017-06-22 20:49:02 +1000

    Updated benchmark to use multiple method calls

--- benchmark.rb
+++ benchmark.rb
@@ -2,12 +2,17 @@ require 'benchmark'
 require_relative 'lib/arcade.rb'
 
 class TestClass
-  attr_accessor :name, :age, :email
+  attr_accessor :name, :age, :email, :var1, :var2
 
   def test(name, age, email)
     @name = name
     @age = age
     @email = email
+  end
+
+  def other_method(var1, var2)
+    @var1 = var1
+    @var2 = var2
   end
 end
 
@@ -17,17 +22,24 @@ class DesplatterClass
   def test(name, age, email)
   end
 
-  desplat :test
+  def other_method(var1, var2)
+  end
+
+  desplat :test, :other_method
 end
 
 puts Benchmark.measure {
   100_000.times do |x|
-    TestClass.new.test("James", 23, "james@workplacearcade.com")
+    obj = TestClass.new
+    obj.test("James", 23, "james@workplacearcade.com")
+    obj.other_method(10, 15)
   end
 }
 
 puts Benchmark.measure {
   100_000.times do |x|
-    DesplatterClass.new.test("James", 23, "james@workplacearcade.com")
+    obj = DesplatterClass.new
+    obj.test("James", 23, "james@workplacearcade.com")
+    obj.other_method(10, 15)
   end
 }
