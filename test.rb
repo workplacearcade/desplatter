60b7b38eb2eb876f847df75d15bcd4e5d388b698
James McLaren <jamesmclaren555@gmail.com>
2017-11-22 22:06:55 +1100

    Tidied up test file

--- test.rb
+++ test.rb
@@ -4,30 +4,11 @@ class Test
   prepend Desplatter
 
   def set_values(name, age, email: 'test@workplacearcade.com')
+    email = 'another_test@workplacearcade.com'
     desplat binding
   end
 end
 
-
-class OldTest
-  attr_accessor :name, :age, :email
-
-  def initialize(name, age, email)
-    @name = name
-    @age = age
-    @email = email
-    test
-  end
-
-  def test
-    puts name
-    puts age
-    puts email
-  end
-end
-
-puts "We are creating a new Test instance"
 test = Test.new
 test.set_values('James McLaren', 20)
 puts test.email
-# OldTest.new('James McLaren', 20, "james@workplacearcade.com")
